568cf42bd99399a26c5ed60d30e1578af3783b95
rickardlarsson22 <rickardlarsson22@gmail.com>
2022-10-18 16:06:52 +0100

    Add all my files

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -1,31 +1,101 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4;
 
 import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
-
+  // VARIABLES
+  /// External contract that will hold staked funds if threshold reached
   ExampleExternalContract public exampleExternalContract;
+
+  /// Balances of user's staked funds
+  mapping (address => uint256) public balances;
+
+  /// Staking threshold
+  uint256 public constant threshold = 1 ether;
+
+  /// Staking deadline
+  uint256 public deadline = block.timestamp + 72 hours;
+
+  /// Boolean set if threshold is not reached by the deadline
+  bool public openForWithdraw;
+
+  // EVENTS
+  /// Emit when stake() is called
+  event Stake(address sender, uint256 value);
+
+  // MODIFIERS
+  /// Modifier that checks whether the required deadline has passed
+  modifier deadlinePassed(bool requireDeadlinePassed) {
+    uint256 timeRemaining = timeLeft();
+    if (requireDeadlinePassed) {
+      require(timeRemaining <= 0, "Deadline has not been passed yet");
+    } else {
+      require(timeRemaining > 0, "Deadline is already passed");
+    }
+    _;
+  }
+
+  /// Modifier that checks whether the external contract is completed
+  modifier notCompleted() {
+    bool completed = exampleExternalContract.completed();
+    require(!completed, "Staking period has completed");
+    _;
+  }
 
   constructor(address exampleExternalContractAddress) {
       exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
   }
 
   // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  // ( Make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+  function stake() public payable deadlinePassed(false) notCompleted {
+    // update the sender's balance
+    balances[msg.sender] += msg.value;
+
+    // emit Stake event to notify the UI
+    emit Stake(msg.sender, msg.value);
+  }
 
 
   // After some `deadline` allow anyone to call an `execute()` function
-  // If the deadline has passed and the threshold is met, it should call `exampleExternalContract.complete{value: address(this).balance}()`
+  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+  function execute() public notCompleted {
+    uint256 contractBalance = address(this).balance;
+    if (contractBalance >= threshold) {
+      // if the `threshold` is met, send the balance to the externalContract
+      exampleExternalContract.complete{value: contractBalance}();
+    } else {
+      // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+      openForWithdraw = true;
+    }
+  }
 
 
-  // If the `threshold` was not met, allow everyone to call a `withdraw()` function to withdraw their balance
 
+  // Add a `withdraw(address payable)` function lets users withdraw their balance
+  function withdraw() public deadlinePassed(true) notCompleted {
+      require(openForWithdraw, "Not open for withdraw");
+      uint256 userBalance = balances[msg.sender];
+      require(userBalance > 0, "userBalance is 0");
+      balances[msg.sender] = 0;
+      (bool sent, ) = msg.sender.call{value: userBalance}("");
+      require(sent, "Failed to send to address");
+  }
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  /// Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+  function timeLeft() public view returns (uint256) {
+      if (block.timestamp >= deadline) {
+          return 0;
+      } else {
+          return deadline - block.timestamp;
+      }
+  }
 
 
   // Add the `receive()` special function that receives eth and calls stake()
-
+  receive() external payable {
+      stake();
+  }
 }
--- packages/hardhat/hardhat.config.js
+++ packages/hardhat/hardhat.config.js
@@ -25,7 +25,7 @@ const { isAddress, getAddress, formatUnits, parseUnits } = utils;
 //
 // Select the network you want to deploy to here:
 //
-const defaultNetwork = "localhost";
+const defaultNetwork = "goerli";
 
 const mainnetGwei = 115;
 
@@ -86,7 +86,7 @@ module.exports = {
       },
     },
     goerli: {
-      url: "https://goerli.infura.io/v3/ec6a8acd1d354717acec099ad46a0bab", // <---- YOUR INFURA ID! (or it won't work)
+      url: "https://eth-goerli.g.alchemy.com/v2/9ddR5hZ7msdcFS5nz2bfUylpRCbAEV2q", // <---- YOUR INFURA ID! (or it won't work)
       //      url: "https://speedy-nodes-nyc.moralis.io/XXXXXXXXXXXXXXXXXXXXXXXXX/eth/goerli", // <---- YOUR MORALIS ID! (not limited to infura)
       accounts: {
         mnemonic: mnemonic(),
